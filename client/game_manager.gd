ad289f34051a65f8465a4734399f86b09e4c8070
ghorshy <ghorshy@protonmail.com>
2025-10-10 00:08:26 +0200

    Client-side state machine

---
+++ client/game_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+enum State {
+	ENTERED,
+	INGAME,
+}
+
+var states_scenes: Dictionary[State, String] = {
+	State.ENTERED: "",
+	State.INGAME: ""
+}
+
+var client_id: int
+var current_scene_root: Node
+
+
+func set_state(state: State) -> void:
+	if current_scene_root != null:
+		current_scene_root.queue_free()
+		
+	var scene: PackedScene = load(states_scenes[state])
+	current_scene_root = scene.instantiate()
+	
+	add_child(current_scene_root)
